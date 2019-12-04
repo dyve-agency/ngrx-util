eab7337e292a9f07cc046c26a71340f0596da132
Marian Theisen <marian@cice-online.net>
2019-12-04 15:35:42 +0100

    fix storing of last params

--- lib/load-data/creators.ts
+++ lib/load-data/creators.ts
@@ -19,10 +19,11 @@ export function createLoadReducer<T, S, P = void>(actions: LoadActions<T, P>): O
       loading: true,
       loadingParams: action.params,
     })),
-    on<LoadActions<T, P>['success'], S>(actions.success, (state: S, action: LoadActionPayload<T> & Action) => ({
+    on<LoadActions<T, P>['success'], S>(actions.success, (state: S, action: LoadActionPayload<T> & ParamsPayload<P> & Action) => ({
       ...state,
       loading: false,
       loadingParams: undefined,
+      lastParams: action.params,
       loaded: true,
       results: action.data,
     })),
